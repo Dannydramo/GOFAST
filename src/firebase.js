954fd7746a94c94528c8299f4a61b33bb5c88b62
Dannydramo <alumandudaniel217@gmail.com>
2023-04-17 06:38:07 -0700

    committed

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAhgNE2w7qEmzCy9ZkATOaOZH-B2HBU9W0",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSANGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: "gofast-auth.firebaseapp.com",
+  databaseURL: "https://gofast-auth-default-rtdb.firebaseio.com",
+  projectId: "gofast-auth",
+  storageBucket: "gofast-auth.appspot.com",
+  messagingSenderId: "539601556122",
+  appId: "1:539601556122:web:2274fff02274186cdc8523",
 };
 
 // Initialize Firebase
